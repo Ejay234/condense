a263244f1660dca36a9ceabe24770ad66d88ec8d
Ejay <ejay.aguirre04@gmail.com>
2022-10-09 19:31:04 +1000

    initailize deployment

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def index():
     return render_template('index.html', answers=condense_sentences)
 
 
-@app.route("/condense", methods=('POST', 'GET'))
+@app.route("/", methods=('POST', 'GET'))
 def condense():
     if request.method == 'POST':
         prompt = request.form['prompt']
